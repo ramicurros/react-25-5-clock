0e54efcfe4523d14142aa672de62f3613b4ccd99
ramicurros <ramicurros@gmail.com>
2023-02-13 20:45:08 -0300

    25 +5 clock layout build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import TimerSettings from './components/timerSettings';
+import TimeDisplay from './components/timeDisplay';
+import Buttons from './components/buttons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='App' className='d-flex  app'>
+      <div id='clock'>
+        <div id='timer-settings'>
+          <TimerSettings />
+        </div>
+        <div id='time-display'>
+          <TimeDisplay />
+        </div>
+        <div id='buttons'>
+          <Buttons />
+        </div>
+      </div>
     </div>
   );
 }
